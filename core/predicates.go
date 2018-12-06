f36adfcfddbf4f3095342a89ac7f05daad059cb4
GoLangsam <GoLangsam.APA@a-bc.net>
2018-12-06 17:36:38 +0100

    new: IsEqual, and: it's predicate, not attribute

--- core/predicates.go
+++ core/predicates.go
@@ -6,7 +6,7 @@ package core
 
 // ===========================================================================
 
-// IsKind returns a function to discriminate
+// IsKind returns a predicate to discriminate
 // whether a given Pair is a kind.
 func IsKind() (pairIs func(Pair) bool) {
 
@@ -16,12 +16,25 @@ func IsKind() (pairIs func(Pair) bool) {
 	}
 }
 
-// IsNested returns a function to discriminate
+// IsNested returns a predicate to discriminate
 // whether a given Pair is a nested pair of pairs.
 func IsNested() (pairIs func(Pair) bool) {
 
 	return func(a Pair) (isNest bool) {
 		_, isNest = a.(nest)
 		return
+	}
+}
+
+// IsEqual returns a predicate to discriminate
+// whether a given Pair is equal to the original Pair a.
+func IsEqual(a Pair) (pairIs func(Pair) bool) {
+	aten, apep := a.Both()
+	return func(item Pair) (isEqual bool) {
+		// TODO: Do we need reflect.DeepEqual here?
+		if item == a { return true}
+		iten, ipep := item.Both()
+		if aten == iten && apep == ipep { return true}
+		return false
 	}
 }
