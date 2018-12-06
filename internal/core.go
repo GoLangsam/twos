2b3e51abfccb6ba107d96c89c350e83c70384acd
GoLangsam <GoLangsam.APA@a-bc.net>
2018-12-06 17:47:39 +0100

    only what we really need

--- internal/core.go
+++ internal/core.go
@@ -23,49 +23,8 @@ type ID = core.ID
 // (Well, programmers prefer offsets over ordinals).
 type Index = core.Index
 
-// Indices represents a stream of ordinal numbers
-// implemented as a (read-only) channel of Index
-// which can be ranged over.
-// type Indices = core.Indices
-
-/*
-// At returns the Index corresponding to slice-offset i: i+1
-func At(i int) Index {
-	return core.At(i)
-}
-*/
-
-// ===========================================================================
-
 // Cardinality represents a cardinal number such as the #-of items in a Pile.
 type Cardinality = core.Cardinality
-
-// Cardinalities represents a stream of cardinal numbers
-// implemented as a (read-only) channel of Cardinality
-// which can be ranged over.
-// type Cardinalities = core.Cardinalities
-
-// ===========================================================================
-
-// Type is the reflect.Type
-type Type = core.Type
-
-/*
-// TypeOf returns the Type of a
-func TypeOf(a interface{}) Type {
-	return core.TypeOf(a)
-}
-*/
-
-// ===========================================================================
-
-// Pair has two sides: Aten & Apep. It may be atomic, or composed, nested.
-type Pair = core.Pair
-
-// Pairs represents a stream of pairs
-// implemented as a (read-only) channel of Pair
-// which can be ranged over.
-// type Pairs core.Pairs
 
 // ===========================================================================
 
@@ -79,52 +38,17 @@ type Head = core.Head
 // a Head and to another Tail.
 type Tail = core.Tail
 
-// NilTail is a helper, it returns the Tail which evaluates to nil, nil.
-//  Note: this Tail is unique. So its the tail, not a tail.
-func NilTail() Tail { return core.NilTail() }
-
 // ===========================================================================
 // Interfaces
 
-// Pile holds Length items.
-type Pile interface {
-	Length() Cardinality
-}
-
-// Named represents a named type.
-type Named interface {
-	Name() string
-}
-
-// Indexed allows to retrieve a Head Of any item by Index.
-type Indexed interface {
-	Of(Index) Head
-}
-
-// Iterable represents a tailor-made collection, a thread for a tailor.
-type Iterable interface {
-	Tail() Tail
-}
-
-// Container can tell for any item whether it contains this item or not.
-type Container interface {
-	Contains(item interface{}) bool
-}
+// Pair has two sides: Aten & Apep. It may be atomic, or composed, nested.
+type Pair = core.Pair
 
 // ===========================================================================
 
-// Pairs represents a stream of pairs
-// implemented as a (read-only) channel of Pair
-// which can be ranged over.
-type Pairs <-chan Pair
-
-// ===========================================================================
-
-// StringOfTwos returns the string of two items.
-func StringOfTwos(a, b interface{}) string { return core.StringOfTwos(a, b) }
-
-// StringOfOnes returns the string of one item.
-func StringOfOnes(a interface{}) string { return core.StringOfOnes(a) }
-
-// StringOfPair returns the string of a Pair
-func StringOfPair(a Pair) string { return core.StringOfPair(a) }
+var (
+	NilTail = core.NilTail
+	StringOfTwos = core.StringOfTwos
+	StringOfOnes = core.StringOfOnes
+	StringOfPair = core.StringOfPair
+)
