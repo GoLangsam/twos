0ff53dd2ffca16dcc47f723ff97655815570dc13
GoLangsam <GoLangsam.APA@a-bc.net>
2018-12-08 12:25:03 +0100

    new: fmap/

---
+++ fmap/genny.go
@@ -0,0 +1,14 @@
+// Copyright 2018 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file uses geanny to pull the type specific generic code
+
+// core datatypes: nest,kind,Cardinality,Index,Name
+// core functypes: Head,Tail
+// core interface: Iterable,Kind,Type,Pair
+// void interface: interface{}
+
+//go:generate genny	-in ..\internal\fmap.go		-out gen.fmap.go	-pkg fmap gen "anyType=interface{},Cardinality,Index,ID,Type,Iterable,Pair,Pile"
+
+package fmap
