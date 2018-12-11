a2c2f10e4fc18781632159433f1632b57ffa6339
GoLangsam <GoLangsam.APA@a-bc.net>
2018-12-11 11:54:54 +0100

    ugly gofmt :-(

--- math/permute.go
+++ math/permute.go
@@ -14,8 +14,8 @@ type permuter struct {
 	reset TailS // for reset
 	TailS       // where we are
 	HeadS       // what we have
-	many int    // how many tails
-	curr int    // where to progress
+	many  int   // how many tails
+	curr  int   // where to progress
 }
 
 // Permuter provides an (opaque and lazy) iterator.
@@ -24,7 +24,9 @@ type permuter struct {
 // reachable thru the given TailS.
 func Permuter(tails TailS) *permuter {
 	many := len(tails)
-	if many < 1 {return nil}
+	if many < 1 {
+		return nil
+	}
 
 	a := &permuter{
 		tails,
@@ -44,11 +46,15 @@ func Permuter(tails TailS) *permuter {
 }
 
 // Next iterates: returns another HeadS and reports iff there is such.
-func (a *permuter)Next() (HeadS, bool) {
-	if len(a.HeadS) < 1 {return nil, false}
+func (a *permuter) Next() (HeadS, bool) {
+	if len(a.HeadS) < 1 {
+		return nil, false
+	}
 
 	headS := HeadS{}
-	for _, head := range a.HeadS {headS = append(headS, head)} // copy(headS, a.HeadS)
+	for _, head := range a.HeadS {
+		headS = append(headS, head)
+	} // copy(headS, a.HeadS)
 	// Note: Intentionally use of copy is avoided: it would need to specify the underlying type.
 
 	a.next(a.curr)
@@ -57,7 +63,7 @@ func (a *permuter)Next() (HeadS, bool) {
 }
 
 // Upto resets tails up to (but not including) given offset.
-func (a *permuter)upto(upto int) *permuter {
+func (a *permuter) upto(upto int) *permuter {
 	for curr := 0; curr < upto && curr < len(a.HeadS); curr++ {
 		a.HeadS[curr], a.TailS[curr] = a.reset[curr].Tail()() // reset all lower
 	}
@@ -67,17 +73,17 @@ func (a *permuter)upto(upto int) *permuter {
 
 // Next evaluates the Tail here and carries over if need.
 // On overflow, the output buffer is shrunk to zero size.
-func (a *permuter)next(here int) *permuter {
+func (a *permuter) next(here int) *permuter {
 	if here < len(a.HeadS) {
 
 		a.HeadS[here], a.TailS[here] = a.TailS[here].Tail()()
 		if a.HeadS[here] == nil {
 			here++
-			if here >= a.many { // too many:
+			if here >= a.many { // too many: Overflow!
 				a.HeadS = HeadS{} // clear output buffer
 				return a
 			}
-			a.curr = 0 // reset position
+			a.curr = 0                           // reset position
 			for curr := 0; curr < here; curr++ { // reset all lower
 				a.HeadS[curr], a.TailS[curr] = a.reset[curr].Tail()()
 			}
